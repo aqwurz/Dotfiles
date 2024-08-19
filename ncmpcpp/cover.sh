28bf2a851c7e2643aaeba7eb62b7b4f15792c8ce
antoine <aqwurz@github.com>
2024-08-19 12:11:30 +0200

    fix song cover script

--- ncmpcpp/cover.sh
+++ ncmpcpp/cover.sh
@@ -1,1 +1,1 @@
-ln -sf "$(ls -rt -d -1 "$(mpc current -f "/home/antoine/Music/%file%" | sed "s/\/[^\/]*\.mp3//g")"/* | grep cover)" /tmp/mpd_cover.jpg
+ln -sf "$(ls -rt -d -1 "$(mpc current -f "/home/antoine/Music/%file%" | sed "s/\/[^\/]*\.\(mp3\|wma\|wav\|ogg\|m4a\)//g")"/* | grep cover | head -1)" /tmp/mpd_cover.jpg
